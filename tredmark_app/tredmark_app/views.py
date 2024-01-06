928c39326b5b3650d001a00698a9f6e6f86510ea
Devendra Bobde <devendrabobde89@gmail.com>
2024-01-06 20:52:21 +0530

    Setup bootstrap template

--- tredmark_app/tredmark_app/views.py
+++ tredmark_app/tredmark_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
   return render(request, 'index.html', context={})
+
+def about(request):
+  return render(request, 'about.html', context={})
--- tredmark_app/tredmark_webapp/views.py
+++ tredmark_app/tredmark_webapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def homepage(request):
   return render(request, 'index.html', context={})
+
+def about(request):
+  return render(request, 'about.html', context={})
