abec763b9429ce7fcdc7b68af1052b1a34df1f26
Devendra Bobde <devendrabobde89@gmail.com>
2024-01-06 21:35:39 +0530

    Setup bootstrap template

--- tredmark_app/tredmark_webapp/urls.py
+++ tredmark_app/tredmark_webapp/urls.py
@@ -3,5 +3,11 @@ from . import views  #importing our view file
 
 urlpatterns = [
     path("", views.homepage, name="home"), #mapping the homepage function
-    path("about.html", views.about, name="about"),
+    path("about", views.about, name="about"),
+    path("appointment", views.appointment, name="appointment"),
+    path("contact", views.contact, name="contact"),
+    path("price", views.price, name="price"),
+    path("service", views.service, name="service"),
+    path("team", views.team, name="team"),
+    path("testimonial", views.testimonial, name="testimonial"),
 ]
--- tredmark_app/tredmark_webapp/views.py
+++ tredmark_app/tredmark_webapp/views.py
@@ -7,3 +7,21 @@ def homepage(request):
 
 def about(request):
   return render(request, 'about.html', context={})
+
+def appointment(request):
+  return render(request, 'appointment.html', context={})
+
+def contact(request):
+  return render(request, 'contact.html', context={})
+
+def price(request):
+  return render(request, 'price.html', context={})
+
+def service(request):
+  return render(request, 'service.html', context={})
+
+def team(request):
+  return render(request, 'team.html', context={})
+
+def testimonial(request):
+  return render(request, 'testimonial.html', context={})
