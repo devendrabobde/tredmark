112f88045a661ac339109bf45d5d766400ed61e7
Devendra Bobde <devendrabobde89@gmail.com>
2024-01-07 18:39:29 +0530

    Integrated contact us feature

--- tredmark_app/tredmark_app/settings.py
+++ tredmark_app/tredmark_app/settings.py
@@ -144,3 +144,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'devendrabobde89@gmail.com'
+EMAIL_HOST_PASSWORD = ''
